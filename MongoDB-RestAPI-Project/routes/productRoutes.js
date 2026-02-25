d8925e1a2bb0573a583194b287ea68c26084d3c4
Shravya <shravyan2005@gmail.com>
2026-02-25 10:33:09 +0530

    MongoDB-RESTAPI

--- MongoDB-RestAPI-Project/routes/productRoutes.js
+++ MongoDB-RestAPI-Project/routes/productRoutes.js
@@ -5,14 +5,46 @@ const Product = require("../models/Product");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  const product = await Product.find();
+  res.json(product);
 });
 
 router.get("/:id", async (req, res) => {
   const product = await Product.findById(req.params.id);
-  if (!Product) {
+  if (!product) {
     return res.json({ message: "Product not found" });
   }
   res.json(product);
 });
+
+router.post("/", async (req, res) => {
+  const product = new Product(req.body);
+  const savedProduct = await product.save();
+  res.json(savedProduct);
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updated) return res.status(404).json({ message: "Product not found" });
+    res.json(updated);
+  } catch {
+    if (updated) return res.json({ message: "Product updated" });
+    res.json(updated);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const deleted = await Product.findByIdAndDelete(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!deleted) return res.status(404).json({ message: "Product not found" });
+  else return res.json({ message: "Product deleted" });
+  res.json(deleted);
+});
+
+module.exports = router;
--- MongoDB-RestAPI-Project/server.js
+++ MongoDB-RestAPI-Project/server.js
@@ -6,3 +6,17 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 const cors = require("cors");
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo Connected"))
+  .catch((err) => console.log(err));
+app.use("/product", require("./routes/productRoutes"));
+
+const PORT = process.env.PORT || 3006;
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
